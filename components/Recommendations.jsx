c2cd09798c57486ef4842e8ad1b055178e0a6ffb
Jasmine <jasmineqw45@gmail.com>
2024-03-03 00:44:42 -0800

    merge

--- components/Recommendations.jsx
+++ components/Recommendations.jsx
@@ -3,7 +3,7 @@ import { Icon } from "@rneui/themed";
 export default function Recommendations({ message }) {
   return (
     <View className="p-5 bg-white w-full items-center rounded-2xl">
-      <View className="flex-row justify-center items-center justify-items-center gap-2 text-">
+      <View className="flex-row justify-center items-center justify-items-center gap-2 text-black opacity-50">
         <Icon name="sparkles" type="ionicon" color="#545454" />
 
         <Text className="font-semibold">Recommendations</Text>
--- components/SummaryElement.jsx
+++ components/SummaryElement.jsx
@@ -2,9 +2,9 @@ import { View, Text } from "react-native";
 import Recommendations from "@/components/Recommendations";
 export default function SummaryElement({ summarytitle }) {
   return (
-    <View className="p-5 bg-white w-full items-center rounded-2xl">
-      <Text className="text-4xl">{summarytitle}</Text>
-      <Recommendations/>
+    <View className="p-5 bg-white w-full items-center rounded-2xl outline outline-2 outline-">
+      <Text className="text-4xl font-extrabold">{summarytitle}</Text>
+      <Recommendations />
     </View>
   );
 }
