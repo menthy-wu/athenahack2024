6446b32bfa28e1f9983515b4957a3cca825d650a
Shing Hung <yhung022@ucr.edu>
2024-03-03 08:39:27 -0800

    calling

--- app/(home)/index.jsx
+++ app/(home)/index.jsx
@@ -18,7 +18,7 @@ export default function TabOneScreen() {
             <FontAwesome
               name="play"
               size={25}
-              color={Colors[colorScheme ?? "light"].text}
+              color="#FFF"
             />
             <Text className=" text-white text-2xl font-semibold"> “Start Route” </Text>
               </View>
