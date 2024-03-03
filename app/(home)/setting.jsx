af7b29506539df2b8cac75707d01eb0dc7beb231
Shing Hung <yhung022@ucr.edu>
2024-03-03 00:44:09 -0800

    add Header

--- app/(home)/setting.jsx
+++ app/(home)/setting.jsx
@@ -1,4 +1,3 @@
-import { StyleSheet } from "react-native";
 import { Text, View } from "react-native";
 
 export default function SettingScreen() {
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,23 @@ module.exports = {
  content: [ "./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}", "./screens/**/*.{js,jsx,ts,tsx}"],
  
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        athena: {
+          "gray-100": "#EFEFF2",
+          "gray-200": "#E7E7EB",
+          "gray-500": "#545454",
+          "light-purple":"#F8EBFF",
+          "bright-purple": "#CC7AFF",
+          "light-red":"#FFEBEB",
+          red:"#FF7A7A"
+        },
+      },
+      fontFamily: {
+        
+      },
+
+    },
   },
   plugins: [],
 }
